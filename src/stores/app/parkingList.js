131b783708200130047c0650840ad1bfe8c33149
Artem Stepanov <temka.do@gmail.com>
2022-09-03 21:28:42 +0300

    Фикс бага в createParkingList

--- src/stores/app/parkingList.js
+++ src/stores/app/parkingList.js
@@ -3,13 +3,9 @@ import { derived } from "svelte/store";
 import parkingPlaces from "./parkingPlaces";
 import parkingListFromDatabase from "./parkingListFromDatabase";
 
-async function fetchParkingData(searchDate) {
-  return new Promise((resolve) => {});
-}
-
 function createParkingList([$parkingPlaces, $parkingListFromDatabase]) {
   return Object.values($parkingPlaces).map(({ level, number }) => {
-    const placeInDatabase = $parkingListFromDatabase.find(
+    const placeInDatabase = $parkingListFromDatabase?.find(
       ({ parkingId }) => +parkingId === +number
     );
 

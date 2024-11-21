a683a6c53bcfb956e61f1cbcbffc17dc9e1fefa3
Artem Stepanov <artyom.stepanov@openmedia.co>
2024-11-21 20:03:58 +0300

    Вернул код

--- src/App.svelte
+++ src/App.svelte
@@ -18,12 +18,6 @@
     onAuthStateChanged($fbAuth, (user) => {
       $userInformation = user;
       isAuthWasChecked = true;
-
-      updateProfile(user, {
-            displayName: "Артем Степанов"
-          }).catch((error) => {
-            console.error(error);
-          });
     });
   })
 </script>
